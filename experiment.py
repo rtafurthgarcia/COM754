94cd45dbc38dfe269213f4830d7e3bd0a8afa57f
rtafurthgarcia <111584774+rtafurthgarcia@users.noreply.github.com>
2025-12-26 09:26:21 +0100

    Added enhanced prompts and made the function for agentic request more generic

--- experiment.py
+++ experiment.py
@@ -1,6 +1,7 @@
 import os 
 import subprocess
 import re
+from typing import Literal
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 import azure.cognitiveservices.speech as speechsdk
@@ -8,6 +9,67 @@ from collections import OrderedDict
 from openai import OpenAI
 from azure.communication.callautomation import CallAutomationClient, CommunicationIdentifier
 from azure.communication.identity import CommunicationIdentityClient, CommunicationUserIdentifier
+from pydantic import BaseModel
+
+class FinalDetectorResults(BaseModel):
+    answer: Literal["SAFE", "FRAUD", "UNCERTAIN"]
+
+class IntermediateEnhancedDetectorResults(BaseModel):
+    answer: bool
+
+naive_prompt = """
+    Please analyze the call content and detect whether it is a fraud call. 
+    Please carefully analyze the suspicious features in the conversation. 
+    If it is a fraud call, please only return "FRAUD". 
+    If it is a normal call, please only return "SAFE". 
+    If there is insufficient information (e.g., it is not yet obvious that the fraud is present), 
+    please return "UNCERTAIN". Do not return anything else.
+"""
+
+authority_prompt = """
+    Please analyze the call content and detect whether the caller is using the "Authority" persuasion principle common in voice phishing. 
+    You can determine so by answering those questions:
+        - Does the caller claim to have authority over the callee?
+        - Does the caller claim to have authority to access the information requested?
+        - Does the caller claim to be a member of a reputable institution?
+        - Is it reasonable for the callee to believe that failure to comply with the caller's request will result in repercussions (e.g. loss of privileges, humiliation, condemnation) based on the caller's supposed authority?
+    If one or many seems to be true, return "TRUE", otherwise, return "FALSE".
+"""
+
+social_proof_prompt = """
+    Please analyze the call content and detect whether the caller is using the "Social proof" persuasion principle common in voice phishing. 
+    You can determine so by answering those questions:
+        - Is it reasonable for the callee to believe that complying with the caller's request will have benefits (including helping the caller)?
+        - Is it reasonable for the callee to believe that they will not be held solely responsible for any negative effects related to complying with the caller's request?
+        - Is it reasonable for the callee to believe that any risk associated with helping the caller is shared by other people as well?
+        - Does the caller state or imply that the callee's peers have helped the caller in this manner in the past?
+        - Is it otherwise reasonable for the callee to believe that it is socially correct to help the caller?
+        - Does the caller state or imply that if the callee does not comply with their request then the callee will be “left out” in some way?
+    If one or many seems to be true, return "TRUE", otherwise, return "FALSE".
+"""
+
+distraction_prompt = """
+    Please analyze the call content and detect whether the caller is using the "Distraction" persuasion principle common in voice phishing. 
+    You can determine so by answering those questions:
+        - Does the caller do anything to heighten the callee's emotional state (e.g. stress, surprise, anger, excitement)?
+        - Does the caller give the callee more information than they can process?
+        - Does the caller state or imply that the information they are requesting is time-sensitive?
+        - Does the caller state or imply that they are in a hurry or otherwise have limited time to converse with the callee?
+        - Does the caller state or imply that there is some benefit to complying with their request but that this benefit is of limited quantity?
+        - Does the caller state or imply that if the callee does not comply with their request then the callee will be “left out” in some way?
+        - Does the caller attempt to distract the callee from thinking about the intentions or consequences related to the caller's request?
+        - Is it reasonable for the callee to believe that if they comply with the caller's request that they will personally benefit from it?
+        - Does the caller state or imply that the consequences of the callee's actions are large?
+        - Is it reasonable for the callee to believe that if they do not comply with the caller's request that they will suffer negative consequences because of it?
+    If one or many seems to be true, return "TRUE", otherwise, return "FALSE".
+"""
+
+"""if (results.authority_detected or results.distraction_detected or results.social_proof_detected) and results.sensitive_requested:
+return FinalDetectorResults(answer="FRAUD")
+elif (results.authority_detected or results.distraction_detected or results.social_proof_detected) or results.sensitive_requested:
+return FinalDetectorResults(answer="UNCERTAIN")
+else:
+return FinalDetectorResults(answer="SAFE")"""
 
 class CallerCallee:
     def __init__(self):
@@ -46,11 +108,44 @@ class CallerCallee:
 
         self._start_dev_tunnel()
     
-    def _analyse_call_for_vishing_naive(self, conversation: OrderedDict):
-        raise NotImplementedError
+    def _analyse_call_for_vishing_naive(self, conversation: OrderedDict) -> FinalDetectorResults | None:
+        response = self.ai_client.responses.parse(
+            model=self.MODEL,
+            store=False,
+            reasoning={"effort": "medium"},
+            instructions=naive_prompt,
+            input=[
+                {
+                    "role": "user",
+                    "content": str(conversation)
+                }
+            ],
+            text_format=FinalDetectorResults
+        )
+
+        return response.output_parsed
     
-    def _analyse_call_for_vishing_enhanced(self, conversation: OrderedDict):
-        raise NotImplementedError
+    def _analyse_call_for_vishing(
+        self, 
+        prompt: str, 
+        conversation: OrderedDict,
+        response_format
+    ) -> FinalDetectorResults | None:
+        response = self.ai_client.responses.parse(
+            model=self.MODEL,
+            store=False,
+            reasoning={"effort": "medium"},
+            instructions=prompt,
+            input=[
+                {
+                    "role": "user",
+                    "content": str(conversation)
+                }
+            ],
+            text_format=response_format
+        )
+
+        return response.output_parsed
 
     def _start_dev_tunnel(self):
         self.process = subprocess.Popen(["devtunnel", "host", "-p", "8080"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
@@ -67,11 +162,11 @@ class CallerCallee:
 
 
     def initiate_calls_from(self, src: str):
-        visher_identifier, visher_token = self.cs_client.create_user_and_token(["voip"])
-        victim_identifier, victim_token = self.cs_client.create_user_and_token(["voip"])
+        caller_identifier, visher_token = self.cs_client.create_user_and_token(["voip"])
+        callee_identifier, callee_token = self.cs_client.create_user_and_token(["voip"])
 
-        visher = CommunicationUserIdentifier(id=visher_identifier.raw_id)
-        victim = CommunicationUserIdentifier(id=visher_identifier.raw_id)
+        visher = CommunicationUserIdentifier(id=caller_identifier.raw_id)
+        callee = CommunicationUserIdentifier(id=caller_identifier.raw_id)
 
 app = CallerCallee()
 app.initiate_calls_from("")
